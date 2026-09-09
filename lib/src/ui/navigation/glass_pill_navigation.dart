110525b43db7c9e662ab4b74246ca3983d99423f
Lycra <113378946+Lycra-GX@users.noreply.github.com>
2026-09-10 00:53:12 +0800

    Revert "Revert "Resolve all blockers from PR #5 Copilot review thread""

--- lib/src/ui/navigation/glass_pill_navigation.dart
+++ lib/src/ui/navigation/glass_pill_navigation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import '../../theme/app_theme.dart';
 
 enum AppTab { home, guides, settings, about }
 
